cdb65922ee314dd630af8d7a0216328cf42aee66
VenceslavDed <slav.dedckow@yandex.ru>
2026-02-19 13:30:30 +0700

    lab01

---
+++ lab01/FlightAtmosphere/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace FlightAtmosphere
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.inputSpeed = new System.Windows.Forms.NumericUpDown();
+            this.inputAngle = new System.Windows.Forms.NumericUpDown();
+            this.inputHeight = new System.Windows.Forms.NumericUpDown();
+            this.inputSize = new System.Windows.Forms.NumericUpDown();
+            this.inputWeight = new System.Windows.Forms.NumericUpDown();
+            this.inputDt = new System.Windows.Forms.NumericUpDown();
+            this.btLaunch = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDt)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            this.chart1.Dock = System.Windows.Forms.DockStyle.Top;
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(0, 0);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(1112, 300);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // inputSpeed
+            // 
+            this.inputSpeed.AccessibleDescription = "Скорость";
+            this.inputSpeed.Location = new System.Drawing.Point(435, 306);
+            this.inputSpeed.Name = "inputSpeed";
+            this.inputSpeed.Size = new System.Drawing.Size(120, 20);
+            this.inputSpeed.TabIndex = 1;
+            // 
+            // inputAngle
+            // 
+            this.inputAngle.AccessibleDescription = "Угол";
+            this.inputAngle.Location = new System.Drawing.Point(435, 332);
+            this.inputAngle.Name = "inputAngle";
+            this.inputAngle.Size = new System.Drawing.Size(120, 20);
+            this.inputAngle.TabIndex = 2;
+            // 
+            // inputHeight
+            // 
+            this.inputHeight.AccessibleDescription = "Высота";
+            this.inputHeight.Location = new System.Drawing.Point(435, 362);
+            this.inputHeight.Name = "inputHeight";
+            this.inputHeight.Size = new System.Drawing.Size(120, 20);
+            this.inputHeight.TabIndex = 3;
+            // 
+            // inputSize
+            // 
+            this.inputSize.AccessibleDescription = "Площадь";
+            this.inputSize.Location = new System.Drawing.Point(435, 396);
+            this.inputSize.Name = "inputSize";
+            this.inputSize.Size = new System.Drawing.Size(120, 20);
+            this.inputSize.TabIndex = 4;
+            // 
+            // inputWeight
+            // 
+            this.inputWeight.AccessibleDescription = "Масса";
+            this.inputWeight.Location = new System.Drawing.Point(435, 422);
+            this.inputWeight.Name = "inputWeight";
+            this.inputWeight.Size = new System.Drawing.Size(120, 20);
+            this.inputWeight.TabIndex = 5;
+            // 
+            // inputDt
+            // 
+            this.inputDt.AccessibleDescription = "Шаг dt";
+            this.inputDt.DecimalPlaces = 4;
+            this.inputDt.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            196608});
+            this.inputDt.Location = new System.Drawing.Point(435, 462);
+            this.inputDt.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            this.inputDt.Name = "inputDt";
+            this.inputDt.Size = new System.Drawing.Size(120, 20);
+            this.inputDt.TabIndex = 6;
+            this.inputDt.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            262144});
+            // 
+            // btLaunch
+            // 
+            this.btLaunch.Location = new System.Drawing.Point(728, 358);
+            this.btLaunch.Name = "btLaunch";
+            this.btLaunch.Size = new System.Drawing.Size(75, 23);
+            this.btLaunch.TabIndex = 7;
+            this.btLaunch.Text = "запуск";
+            this.btLaunch.UseVisualStyleBackColor = true;
+            this.btLaunch.Click += new System.EventHandler(this.btLaunch_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 502);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1112, 150);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(573, 306);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "скорость";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(573, 332);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "угол";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(573, 361);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "высота";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(573, 395);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "площадь";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(573, 421);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "масса";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(573, 461);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 14;
+            this.textBox6.Text = "шаг dt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1112, 652);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btLaunch);
+            this.Controls.Add(this.inputDt);
+            this.Controls.Add(this.inputWeight);
+            this.Controls.Add(this.inputSize);
+            this.Controls.Add(this.inputHeight);
+            this.Controls.Add(this.inputAngle);
+            this.Controls.Add(this.inputSpeed);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDt)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.NumericUpDown inputSpeed;
+        private System.Windows.Forms.NumericUpDown inputAngle;
+        private System.Windows.Forms.NumericUpDown inputHeight;
+        private System.Windows.Forms.NumericUpDown inputSize;
+        private System.Windows.Forms.NumericUpDown inputWeight;
+        private System.Windows.Forms.NumericUpDown inputDt;
+        private System.Windows.Forms.Button btLaunch;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ lab01/FlightAtmosphere/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace FlightAtmosphere
+{
+    public partial class Form1 : Form
+    {
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+        public Form1()
+        {
+            InitializeComponent();
+            chart1.ChartAreas[0].AxisX.Title = "x (м)";
+            chart1.ChartAreas[0].AxisY.Title = "y (м)";
+
+            dataGridView1.Columns.Add("dt", "dt");
+            dataGridView1.Columns.Add("dist", "Дальность");
+            dataGridView1.Columns.Add("hmax", "Макс. высота");
+            dataGridView1.Columns.Add("vfinal", "Скорость в конце");
+        }
+
+        private void btLaunch_Click(object sender, EventArgs e)
+        {
+            double v0 = (double)inputSpeed.Value;
+            double angle = (double)inputAngle.Value * Math.PI / 180.0;
+            double h0 = (double)inputHeight.Value;
+            double S = (double)inputSize.Value;
+            double m = (double)inputWeight.Value;
+            double dt = (double)inputDt.Value;
+
+            double k = 0.5 * C * rho * S / m;
+
+            Simulate(v0, angle, h0, k, dt);
+        }
+        private void Simulate(double v0, double angle, double h0, double k, double dt)
+        {
+            double x = 0;
+            double y = h0;
+            double vx = v0 * Math.Cos(angle);
+            double vy = v0 * Math.Sin(angle);
+            double maxHeight = y;
+
+            Series series = new Series("dt=" + dt);
+            series.ChartType = SeriesChartType.Line;
+            series.BorderWidth = 2;
+            chart1.Series.Add(series);
+
+            while (y > 0)
+            {
+                series.Points.AddXY(x, y);
+
+                RungeKuttaStep(ref x, ref y, ref vx, ref vy, k, dt);
+
+                if (y > maxHeight)
+                    maxHeight = y;
+            }
+
+            double finalSpeed = Math.Sqrt(vx * vx + vy * vy);
+
+            dataGridView1.Rows.Add(
+                dt,
+                x.ToString("F2"),
+                maxHeight.ToString("F2"),
+                finalSpeed.ToString("F2")
+            );
+        }
+        private void RungeKuttaStep(ref double x, ref double y,
+                                    ref double vx, ref double vy,
+                                    double k, double dt)
+        {
+            double ax(double vx_, double vy_)
+            {
+                double v = Math.Sqrt(vx_ * vx_ + vy_ * vy_);
+                return -k * vx_ * v;
+            }
+
+            double ay(double vx_, double vy_)
+            {
+                double v = Math.Sqrt(vx_ * vx_ + vy_ * vy_);
+                return -g - k * vy_ * v;
+            }
+
+            double k1x = vx;
+            double k1y = vy;
+            double k1vx = ax(vx, vy);
+            double k1vy = ay(vx, vy);
+
+            double k2x = vx + k1vx * dt / 2;
+            double k2y = vy + k1vy * dt / 2;
+            double k2vx = ax(vx + k1vx * dt / 2, vy + k1vy * dt / 2);
+            double k2vy = ay(vx + k1vy * dt / 2, vy + k1vy * dt / 2);
+
+            double k3x = vx + k2vx * dt / 2;
+            double k3y = vy + k2vy * dt / 2;
+            double k3vx = ax(vx + k2vx * dt / 2, vy + k2vy * dt / 2);
+            double k3vy = ay(vx + k2vy * dt / 2, vy + k2vy * dt / 2);
+
+            double k4x = vx + k3vx * dt;
+            double k4y = vy + k3vy * dt;
+            double k4vx = ax(vx + k3vx * dt, vy + k3vy * dt);
+            double k4vy = ay(vx + k3vy * dt, vy + k3vy * dt);
+
+            x += dt / 6 * (k1x + 2 * k2x + 2 * k3x + k4x);
+            y += dt / 6 * (k1y + 2 * k2y + 2 * k3y + k4y);
+            vx += dt / 6 * (k1vx + 2 * k2vx + 2 * k3vx + k4vx);
+            vy += dt / 6 * (k1vy + 2 * k2vy + 2 * k3vy + k4vy);
+        }
+
+    }
+}
